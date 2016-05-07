8ceec924a9ea72561dda7279f9b8d6da8e8f838e
Mohit Agrawal <mohit.agrawal886@gmail.com>
2016-05-07 15:47:56 +0000

    How to create module and use it

---
+++ Create_module/add_module.py
@@ -0,0 +1,4 @@
+def add(a,b):
+	return str(int(a)+int(b))
+def sub(a,b):
+	return str(int(a)-int(b))
---
+++ Create_module/use_of_moudule.py
@@ -0,0 +1,6 @@
+import add_module
+
+a=input("Enter the value of A :")
+b=input("Enter the value of B :")
+
+print "Addition of A and B is "+add_module.add(a,b)
